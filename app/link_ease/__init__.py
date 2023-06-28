0204b0fbdca944b02ffaabf5027684f632fb5aaa
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-06-28 22:00:21 +0100

    Deployment Phase

--- app/link_ease/__init__.py
+++ app/link_ease/__init__.py
@@ -4,14 +4,15 @@ from .routes import cache, limiter
 from .extensions import db, bcrypt, login_manager, migrate
 from .routes import short
 from dotenv import load_dotenv
+from .settings import config_dict
 
 
 login_manager.login_view = 'short.login'
 
 
-def create_app(config_file='settings.py'):
+def create_app(config=config_dict['prod']):
     app = Flask(__name__)
-    app.config.from_pyfile(config_file)
+    app.config.from_object(config)
 
     load_dotenv()
 
--- app/link_ease/settings.py
+++ app/link_ease/settings.py
@@ -1,7 +1,40 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3' #os.environ.get('DATABASE_URL')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = '4128c5e3a66aba8278a0f4523f123d9c'
-ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
-ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3' #os.environ.get('DATABASE_URL')
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SECRET_KEY = '4128c5e3a66aba8278a0f4523f123d9c'
+# ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
+# ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+
+default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', 'link_ease_db')
+
+uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+
+class Config:
+    SECRET_KEY = '4128c5e3a66aba8278a0f4523f123d9c'
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3' #os.environ.get('DATABASE_URL')
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+class ProdConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = uri
+
+
+config_dict = {
+    'dev': DevConfig,
+    'test': TestConfig,
+    'prod': ProdConfig
+}
