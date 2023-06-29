a6db407070e311f247143d9474a8e92d51f53b03
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-06-29 16:26:19 +0100

    Update .flaskenv

--- app/.flaskenv
+++ app/.flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=link_ease
+FLASK_APP=app.py
 FLASK_ENV=development
--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from link_ease import create_app
-from app.link_ease.settings import config_dict
+from link_ease.settings import config_dict
 
 app = create_app(config=config_dict['prod'])
 
